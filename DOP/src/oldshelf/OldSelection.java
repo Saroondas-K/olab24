23ac80d83ef24cc5ddf023d2dace29ad63778283
Saroondas-K <116355315+Saroondas-K@users.noreply.github.com>
2024-11-03 21:05:56 +0530

    Update OldSelection.java

--- DOP/src/oldshelf/OldSelection.java
+++ DOP/src/oldshelf/OldSelection.java
@@ -2,28 +2,23 @@ package oldshelf;
 public class OldSelection {
 
    
-    public static String getAgeOrTitle(Object o) {
-        if (!(o instanceof Book)) {
-            return "";
-        }
-        if (o instanceof Comic) {
-            return ((Comic) o).getTitle();
-        } else if (o instanceof Fiction) {
-            return ((Fiction) o).getName();
-        } else if (o instanceof TextBook) {
-            return ((TextBook) o).getSubject();
-        }
-        return "";
+public static String getAgeOrTitle(Object o) {
+        return switch (o) {
+            case Comic comic -> comic.getTitle();
+            case Fiction fiction -> fiction.getName();
+            case TextBook textBook -> textBook.getSubject();
+            case null,default -> "";
+        };
     }
 
     public static void main(String[] args) {
-        TextBook t = new TextBook("Social Studies");
+        TextBook t = new TextBook("Computer Science");
         System.out.println("Subject of the TextBook: " + getAgeOrTitle(t)); 
         
-        Comic c = new Comic("Dylan:The Great Adventures", 12);
+        Comic c = new Comic("Spiderman", 12);
         System.out.println("Title of the Comic: " + getAgeOrTitle(c));
         
-        Fiction f = new Fiction("Pulp Fiction", FictionType.Comedy);
+        Fiction f = new Fiction("Hangover", FictionType.Comedy);
         System.out.println("Name of the Fiction: " + getAgeOrTitle(f));
     }
 }
