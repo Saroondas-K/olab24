58ec19073f8fb5611fb34b04571ab8828bc2b1e0
Saroondas-K <116355315+Saroondas-K@users.noreply.github.com>
2024-11-03 22:03:59 +0530

    Update TextBook.java

--- DOP/src/newshelf/TextBook.java
+++ DOP/src/newshelf/TextBook.java
@@ -1,8 +1,15 @@
 package newshelf;
 
-public record TextBook(String subject) implements IBook {
+public record TextBook(String subject) implements IBook<String> {
+    
     @Override
     public String getTitle() {
         return subject;
     }
+   @Override
+       public int compareTo(IBook<String> o)
+       {
+
+       return this.subject .compareTo(o.getTitle());
+   }
 }
