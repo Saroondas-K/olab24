35c5c647f98752a754b02a5682ef25687a57cd06
Saroondas-K <116355315+Saroondas-K@users.noreply.github.com>
2024-11-03 21:19:46 +0530

    Update Fiction.java

--- DOP/src/newshelf/Fiction.java
+++ DOP/src/newshelf/Fiction.java
@@ -5,4 +5,9 @@ public record Fiction( String name) implements IBook {
     public String getTitle() {
         return name;
     }
+    @Override
+        public int compareTo(IBook<String> other)
+        {
+        return this.name.compareTo(other.getTitle());
+    }
 }
